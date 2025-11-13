1e7ab9cf8af2c1c5568594b8a5a088b82e7cc785
bkataru <baalateja.k@gmail.com>
2025-11-13 09:32:07 +0000

    gettin my ass cooked cuz ion nuff c3 + lemme try da zig variant again

--- run.zig
+++ run.zig
@@ -22,6 +22,33 @@ const TransformerWeights = struct {
     rms_ffn_weight: []const f32, // (layer, dim)
     // weights for matmuls
     wq: []const f32, // (layer, dim, n_heads * head_dim)
+    wk: []const f32, // (layer, dim, n_kv_heads * head_dim)
+    wv: []const f32, // (layer, dim, n_kv_heads * head_dim)
+    wo: []const f32, // (layer, n_heads * head_dim, dim)
+    wq_norm: []const f32, // (layer, head_dim)
+    wk_norm: []const f32, // (layer, head_dim)
+    // weights for ffn. w1 = up, w3 = gate, w2 = down
+    w1: []const f32, // (layer, dim, hidden_dim)
+    w2: []const f32, // (layer, hidden_dim, dim)
+    w3: []const f32, // (layer, dim, hidden_dim)
+    // final rmsnorm
+    rms_final_weight: []const f32, // (dim,)
+    // Same as token_embedding_table. GGUF has the final layer anyway
+    wcls: []const f32,
+};
+
+const RunState = struct {
+    // current wave of activations
+    x: []const f32, // activation at current time stamp (dim,)
+    xb: []const f32, // buffer (dim,)
+    xb2: []const f32, // an additional buffer just for convenience (dim,)
+    xb3: []const f32, // an additional buffer just for convenience (att_head_dim,)
+    hb: []const f32, // buffer for hidden dimension in the ffn (hidden_dim,)
+    hb2: []const f32, // buffer for hidden dimension in the ffn (hidden_dim,)
+    q: []const f32, // query (att_head_dim,)
+    k: []const f32, // key (dim,)
+    v: []const f32, // value (dim,)
+    att: []const f32, // buffer for scores/attention values (n_heads, seq_len)
 };
 
 pub fn main() void {}
